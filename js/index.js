f89b89e1bde6ec3263670fc6e52992880e9219a2
BennyLouie <bennylouie1412@gmail.com>
2019-10-14 09:36:16 -0400

    created book show display

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+const listPanel = document.querySelector('#list-panel')
+
+fetch('http://localhost:3000/books')
+.then(resp => resp.json())
+.then(obj => obj.forEach(loadBooks))
+});
+
+function loadBooks(obj){
+    const unOrderedList = document.querySelector('#list')
+    let book = document.createElement('li')
+    book.innerText = obj.title
+    book.addEventListener('click', evt => {
+        console.log(obj)
+        let showPanel = document.querySelector('#show-panel')
+        showPanel.innerHTML = ''
+
+        let title = document.createElement('h2')
+        title.innerText = obj.title
+
+        let image = document.createElement('img')
+        image.src = obj.img_url
+
+        let desc = document.createElement('p')
+        desc.innerText = obj.description
+        
+        let users = document.createElement('p')
+        let strong = document.createElement('strong')
+        users.append(strong)
+        
+        let userList = document.createElement('ul')
+        strong.append(userList)
+
+        let usersArray = obj.users
+        usersArray.forEach(user => {let userItem = document.createElement('li'); userItem.innerText = user.username; userList.append(userItem)})
+        
+        let likeBtn = document.createElement('button')
+        likeBtn.innerText = 'Like Book'
+
+        showPanel.append(title, image, desc, users, likeBtn)
+    })
+    unOrderedList.append(book)
+}
